c4952743dc4b292cf04891e8ec806a022b091edc
Herman Yezhela <germanegela@gmail.com>
2025-10-26 16:04:50 +0200

    0004

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import { ContactsCollection } from '../db/models/contact.js';
+import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
@@ -7,5 +7,22 @@ export const getAllContacts = async () => {
 
 export const getContactById = async (id) => {
   const contact = await ContactsCollection.findById(id);
+  return contact;
+};
+
+export const postContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const patchContact = async (id, payload) => {
+  const contact = await ContactsCollection.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return contact;
+};
+
+export const deleteContact = async (id) => {
+  const contact = await ContactsCollection.findByIdAndDelete(id);
   return contact;
 };
